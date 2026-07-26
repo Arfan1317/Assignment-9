297ac531ec70db784559234817f69a958990685f
Arfan1317 <piasharfanulhoque@gmail.com>
2026-07-26 20:12:41 +0600

    updated the page.js file

--- src/app/page.js
+++ src/app/page.js
@@ -1,18 +1,19 @@
 import HeroBanner from "@/components/home/HeroBanner";
 import TopRatedDoctors from "@/components/home/TopRatedDoctors";
 import WhyChooseUs from "@/components/home/WhyChooseUs";
+import CallToAction from "@/components/home/CallToAction";
+import HowItWorks from "@/components/home/HowItWorks";
+import Testimonials from "@/components/home/Testimonials";
 
 export default function Home() {
   return (
-    <div className="min-h-screen">
-      
+    <div className="min-h-screen bg-white">
       <HeroBanner />
-      
-     
       <TopRatedDoctors />
-      
-     
       <WhyChooseUs />
+      <CallToAction />
+      <HowItWorks />
+      <Testimonials />
     </div>
   );
 }
